dd357e6713c5a5b23bd212abcd599542ab55c406
Hunter Kaleb Perry <skittles2519@hotmail.com>
2020-03-15 23:59:12 -0600

    updates

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -6,8 +6,13 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [System.ComponentModel.DataAnnotations.Display(Name = "Last Name")]
+
         public string LastName { get; set; }
+        [System.ComponentModel.DataAnnotations.Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [System.ComponentModel.DataAnnotations.Display(Name = "Enrollment Date")]
+
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
